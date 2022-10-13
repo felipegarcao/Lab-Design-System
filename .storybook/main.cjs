7c4bd64d6d898d93a49a5b14dba12b9c004d511d
Luis Felipe Garção <luis.silva@unimedprudente.com.br>
2022-10-13 12:10:35 -0300

    update deploy docs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,11 +15,11 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, {configType}) => {
-    if(configType === 'PRODUCTION') {
-      config.base = './Lab-Design-System'
-    }
+  // viteFinal: (config, {configType}) => {
+  //   if(configType === 'PRODUCTION') {
+  //     config.base = './Lab-Design-System'
+  //   }
 
-    return config;
-  }
+  //   return config;
+  // }
 }
